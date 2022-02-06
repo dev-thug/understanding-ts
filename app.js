ecd0a77d81b4b474d7d97ca7f45165bffa607fa3
Hyunjoong Kim <de0978@gmail.com>
2022-02-06 23:08:07 +0900

    add objs-arrays-enums

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
-function add(n1, n2, showResult, pharse) {
-    if (showResult) {
-        var result = n1 + n2;
-        console.log(pharse + result);
-    }
-    else {
-        return n1 + n2;
-    }
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 5] = "ADMIN";
+    Role[Role["READ_ONLY"] = 6] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 7] = "AUTHOR";
+})(Role || (Role = {}));
+;
+var person = {
+    name: "Hyunjoong Kim",
+    age: 28,
+    hobbies: ["Sports", "Cooking"],
+    role: Role.AUTHOR
+};
+var favoriteActivities;
+favoriteActivities = ["Sports"];
+console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
 }
-var number1 = 5;
-var number2 = 2.8;
-var printResult = true;
-var resultPhrase = "Result is: ";
-add(number1, number2, printResult, resultPhrase);
+if (person.role === Role.AUTHOR) {
+    console.log('is Author');
+}
